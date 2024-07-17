e9819740ddc1b27f705eb3919814d6f0dd89807c
bishal ad <144100863+Bishal2231@users.noreply.github.com>
2024-07-17 13:19:45 +0545

    Update asynchandler.js

--- src/utlis/asynchandler.js
+++ src/utlis/asynchandler.js
@@ -4,7 +4,7 @@
 export const asynchandler=(requestHandler)=>{
     return(req,res,next)=> {
         Promise.resolve(asynchandler(req,res,next)).catch((err)=>{
-            nexr(err)
+            next(err)
         })
     }
 }
